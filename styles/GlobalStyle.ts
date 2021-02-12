6a2817f0b047c26a996db1e359e97aef8c678e56
programadorNeto <neto.devx@gmail.com>
2021-02-12 11:06:19 -0300

    adding new files

---
+++ styles/GlobalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+  body {
+
+  }
+`;
